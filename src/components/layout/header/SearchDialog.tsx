202c906e7bd024dfc14e9a4614aa092db745a8a8
dinh0107 <duongvandinh.work@gmail.com>
2025-08-28 13:16:05 +0700

    close dialog

--- src/components/layout/header/SearchDialog.tsx
+++ src/components/layout/header/SearchDialog.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogTitle,
   DialogTrigger,
@@ -84,18 +85,20 @@ export default function SearchDialog() {
             <ul className="space-y-2">
               {results.map((movie) => (
                 <li key={movie.id}>
-                  <Link
-                    href={`/movies/${movie.slug}`}
-                    onClick={() => setSearch("")}
-                    className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-gray-800 transition cursor-pointer"
-                  >
-                    <img
-                      src={movie.thumb_url}
-                      alt={movie.name}
-                      className="w-10 h-14 object-cover rounded-md border border-gray-700"
-                    />
-                    <span className="text-white">{movie.name}</span>
-                  </Link>
+                  <DialogClose asChild>
+                    <Link
+                      href={`/movies/${movie.slug}`}
+                      onClick={() => setSearch("")}
+                      className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-gray-800 transition cursor-pointer"
+                    >
+                      <img
+                        src={movie.thumb_url}
+                        alt={movie.name}
+                        className="w-10 h-14 object-cover rounded-md border border-gray-700"
+                      />
+                      <span className="text-white">{movie.name}</span>
+                    </Link>
+                  </DialogClose>
                 </li>
               ))}
             </ul>
